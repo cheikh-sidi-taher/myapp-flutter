19fa0039a62bfa2e2a1399a74dc20cbeff840e4c
cheikh-sidi-taher <cheikh-sidi-taher>
2023-05-05 14:30:09 +0000

    webview

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,5 +1,6 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/modules/web_view/web_view_screen.dart';
 
 Widget DfaultButton({
   double width = double.infinity,
@@ -61,49 +62,55 @@ Widget buildTaskTtem(Map Model) => Padding(
     );
 
 /// components articleItem
-Widget buildArticleItem(article, context) => Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: [
-          Container(
-              width: 120.0,
-              height: 120.0,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  image: DecorationImage(
-                      image: NetworkImage(
-                        '${article['urlToImage']}',
-                      ),
-                      fit: BoxFit.cover))),
-          SizedBox(
-            height: 20.0,
-          ),
-          Expanded(
-            child: Container(
-              height: 120.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      '${article['title']}',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Text(
-                    '${article['publishedAt']}',
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              ),
+Widget buildArticleItem(article, context) => InkWell(
+  onTap: (){
+    
+    navigateTo(context, WebViewScreen(article['url']));
+  },
+  child:   Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Container(
+                width: 120.0,
+                height: 120.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          '${article['urlToImage']}',
+                        ),
+                        fit: BoxFit.cover))),
+            SizedBox(
+              height: 20.0,
             ),
-          )
-        ],
+            Expanded(
+              child: Container(
+                height: 120.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '${article['title']}',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Text(
+                      '${article['publishedAt']}',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    );
+);
 
 /// articleBuilder
 Widget articleBuilder(list, context) => ConditionalBuilder(
